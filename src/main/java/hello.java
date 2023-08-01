c77d970799ac4ac6936ce1eac5b4801c554172b0
Lê Thị Thúy Hằng <ltthang1721@gmail.com>
2023-08-01 15:19:29 +0700

    ok

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -3,6 +3,6 @@ import DAO.*;
 public class hello{
     public static void main(String[] args){
         BookDAO bookDAO = new BookDAO();
-        bookDAO.insertModel(new BookDTO("Book1",10,120000,"book1.png",1,"Id1","Id2","id3","id4"));
+//        bookDAO.insertModel(new BookDTO("Book1",10,120000,"book1.png",1,"Id1","Id2","id3","id4"));
     }
 }
